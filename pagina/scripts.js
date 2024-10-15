6fa1f3742c63ebba315d6c070a735a66c808ccbb
Mathias Baldizon <167600729+onlyxoten@users.noreply.github.com>
2024-10-14 21:27:23 -0600

    Add files via upload

---
+++ pagina/scripts.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío real del formulario
+    alert('Gracias por contactarnos. Te responderemos pronto.');
+});
